74bdd6e7321159a626926fceb8095da674d74f14
jasonwjones <jasonwjones@079ff222-8d0e-11de-b999-7dfecaa767bb>
2009-08-20 22:26:49 +0000

    Added GPL3 notices Deleted some dead blocks of code

--- src/com/jones_systems/TieoutTest.java
+++ src/com/jones_systems/TieoutTest.java
@@ -1,3 +1,24 @@
+/*
+
+	Copyright 2009 Jason Jones
+
+	This file is part of Tieout.
+
+    Tieout is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tieout is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tieout.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package com.jones_systems;
 
 import java.util.Hashtable;
@@ -13,19 +34,13 @@ public class TieoutTest {
 	
 	private Vector<TieoutQuery> queries;
 	private Hashtable<String, TieoutVariableSet> varsets;
-	
-	//private Hashtable<String, Boolean> results;
 	private String name;
-	
-	//private Hashtable<String, Vector<Double>> results;
 	
 	public TieoutTest(String testName) {
 
 		name = testName;
 		queries = new Vector<TieoutQuery>();
 		varsets = new Hashtable<String, TieoutVariableSet>();
-		//results = new Hashtable<String, Boolean>();
-		//results = new Hashtable<String, Vector<Double>>();
 		
 	}
 	
